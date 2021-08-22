81cd2d9a4f98f025d0886e7c0e436016fd290927
HaxSam <56654878+HaxSam@users.noreply.github.com>
2021-08-22 21:32:03 +0200

    fixed close button for mobile

--- src/App.svelte
+++ src/App.svelte
@@ -79,7 +79,7 @@
 			<div class="flex w-screen items-center my-auto">
 				<TodoInput on:create={(event) => addTodo(event.detail)} mobile={true} />
 			</div>
-			<button class="bg-gray-600 absolute right-4 w-8 h-8 rounded-full bottom-96 mb-44" on:click="{closeMobileInput}">
+			<button class="bg-gray-600 absolute right-4 w-8 h-8 rounded-full bottom-96 mb-32" on:click="{closeMobileInput}">
 				<svg class="text-white m-auto fill-current" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
 					<path d="M0 0h24v24H0z" fill="none"/>
 					<path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
